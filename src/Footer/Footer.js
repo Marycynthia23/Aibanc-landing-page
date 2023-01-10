916e725da6effa035483a63871191bd290d344c3
Marycynthia23 <onuohanneoma70@yahoo.com>
2023-01-10 19:11:00 +0100

    merging to master

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css"
 const Footer = () => {
   return (
     <div className='FooterCont'>
-{/* <ul className="navmenu">
+ <ul className="navmenu">
     <li><a href="">Our Company</a></li>
     <li><a href="">Contact us</a></li>
     <li><a href="">FAQs</a></li>
@@ -12,27 +12,27 @@ const Footer = () => {
 </ul>
 <div className="socials">
 
-</div> */}
-<div className="progress">
+</div> 
+{/* <div className="progress">
 <ul>
-    <li>
+    <li className="li">
         <p>1</p>
-        <a href="" lassName="num">5</a>
+        <a href="" className="num">5</a>
         <h6>Select item</h6>
     </li>
-    <li>
+    <li className="li">
         <p>2</p>
-        <a href="" lassName="num">6</a>
+        <a href="" className="num">6</a>
         <h6> item</h6>
     </li>
-    <li>
+    <li className="li">
         <p>3</p>
-        <a href="" lassName="num">7</a>
+        <a href="" className="num">7</a>
         <h6>Select </h6>
     </li>
    
 </ul>
-</div>
+</div> */}
     </div>
   )
 }
