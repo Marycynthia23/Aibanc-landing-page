f7dad52d2a79aa01ea0f7b210dfc0dbc08d31448
ifeanyi-okoronkwo,Tochi <tochi.okoronkwo@interswitchng.com>
2023-01-19 06:11:41 +0100

    corrections

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -11,8 +11,7 @@ function Testimonial() {
       description:
         ' "Awesome and seamless experience. Quick crediting, very low interest rate and user friendly. My number 1 go to reliable finance partner".',
       titlee: "Prince Friday Ocheni",
-      imageSrc:
-        "https://images.pexels.com/photos/3474629/pexels-photo-3474629.jpeg?auto=compress&cs=tinysrgb&w=600",
+      imageSrc: "https://www.usm.edu/news/2022/_images/petron-brown.jpeg",
       alt: "The world",
     },
     {
@@ -20,7 +19,7 @@ function Testimonial() {
         '"Amazing App, easy to use and straight forward. Making access to funds easily ata a favourable interest rate. I have been able to do alot of stuff without breaking the bank, thanks to Aibanc."',
       titlee: "Deborah Ogbole",
       imageSrc:
-        "https://images.pexels.com/photos/3785991/pexels-photo-3785991.jpeg",
+        "https://scontent.flos1-1.fna.fbcdn.net/v/t39.30808-6/310174650_2296836443818199_6670450244339634539_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeFQik5ofL0x7zMhoyOrV7WzM2Ygy2YwNEszZiDLZjA0S9tKSLI5NgTrfe-Tv118UEVHsd06qv4ou8V0UwPS2cdg&_nc_ohc=eWeBK4cf1CsAX8XLoT2&tn=P6-_qwiSieItVHuo&_nc_zt=23&_nc_ht=scontent.flos1-1.fna&oh=00_AfD3AOiKIrfoY7JElA0tp0hIPC6yui7_R-I8wDjCQm_jhA&oe=63CE1514",
     },
     {
       description:
@@ -34,7 +33,7 @@ function Testimonial() {
 
       titlee: "Bakare Noah",
       imageSrc:
-        "https://images.pexels.com/photos/5082976/pexels-photo-5082976.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "https://images.unsplash.com/photo-1615813967515-e1838c1c5116?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
     },
   ];
   return (
@@ -56,7 +55,9 @@ function Testimonial() {
           <div key={index}>
             <div className="test-cont">
               <p className="description">{card.description}</p>
-              <img src={card.imageSrc} alt="" />
+              <span className="circle-image">
+                <img src={card.imageSrc} alt="" />
+              </span>
               <p className="text-cont-title">{card.titlee}</p>
             </div>
           </div>
